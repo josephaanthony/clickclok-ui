7b90a13351611c51a14449adf813390f9a16d6b9
josep <joseph.a@dadyin.com>
2024-05-28 22:59:50 -0400

    dist folder changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.clikclok.ui',
   appName: 'clikclock-ui',
-  webDir: 'www',
+  webDir: 'dist',
   plugins: {
     GoogleAuth: {
       scopes: ['profile', 'email'],
